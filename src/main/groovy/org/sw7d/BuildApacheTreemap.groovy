bf70b5307fcffc9380540f92c4e0011ef324087f
Pawel Slusarz <p@sw7d.com>
2013-10-13 23:01:58 -0500

    a couple more groups and sorted

--- src/main/groovy/org/sw7d/BuildApacheTreemap.groovy
+++ src/main/groovy/org/sw7d/BuildApacheTreemap.groovy
@@ -4,12 +4,12 @@ import groovy.json.JsonSlurper
 
 class BuildApacheTreemap {
 
-  static groups = ['apache', 'webservices', 'tuscany', 'tiles', 'tapestry', 'redback', 'myfaces', 'qpid','karaf', 'jackrabbit', 'harmony', 'directory','activemq',
-          'hadoop', 'manifoldcf', 'servicemix', 'geronimo', 'struts', 'maven', 'log4j', 'tomcat', 'incubator','commons']
+  static groups = ['apache', 'creadur','cxf','webservices', 'tuscany', 'tiles', 'tapestry', 'redback', 'myfaces', 'qpid','karaf', 'directory','activemq','axis',
+          'hadoop','harmony', 'lucene','manifoldcf', 'mina', 'jackrabbit','james','servicemix', 'geronimo', 'struts', 'maven', 'log4j', 'tomcat', 'incubator','commons']
 
   static void main(args) {
       GithubSearchRetriever search = new GithubSearchRetriever(type: 'repositories', query:'@apache+language:java', loadFromFile: true, getAll: true)
-      search.results.items.each {
+      search.results.items.sort{it.name}.each {
           println it.name+"\t"+getGroup(it.name)+"\t"+it.size+"\t"+it.watchers_count
       }
       groups.each {
